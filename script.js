cb01ea0b750b375985103162a601bab9dc33641d
AYUSH <106034287+Ayushrawat777@users.noreply.github.com>
2024-03-31 16:21:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-//your JS code here. If required.
-  function replaceElement() {
-        var newElement = document.createElement('h1');
-         newElement.textContent = 'Entered Metaverse';
-         document.getElementById("status").replaceWith(newElement)
-        }
-        document.getElementById("enterBtn").addEventListener('click', replaceElement)
+ function enter(){
+	document.getElementById('status').innerHTML = '<h1>Entered Metaverse</h1>';
+}
